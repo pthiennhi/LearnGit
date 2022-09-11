bde2c78e9bd23aa41ab2116ba064a31038bbbff6
thien nhi <nhi>
2022-09-11 16:27:08 +0700

    add getter setter

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -5,4 +5,19 @@ public class Student {
     public Student() {
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
